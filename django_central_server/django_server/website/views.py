bd808437f0b7b7426f37f7f7fa0e3450edeff1d2
N.Albertsen <sk74pn@studnet.auu.dk>
2024-10-29 14:12:39 +0100

    lavet logout, og sat alt lidt pænere op, så man bliver redirectet rigtigt

--- django_central_server/django_server/website/views.py
+++ django_central_server/django_server/website/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
+        return render(request, 'home.html', {})
+
+def login_user(request):
     # check if user is authenticated
     if request.method == 'POST':
         email = request.POST['email_address']
@@ -14,15 +17,14 @@ def home(request):
         if user is not None:
             login(request, user)
             messages.success(request, 'You have successfully logged in')
-            return redirect('login')
+            return redirect('home')
         else:
             messages.success(request, 'Invalid username or password')
             return redirect('login')
     else:
-        return render(request, 'home.html', {})
-
-def login_user(request):
-    return render(request, 'login.html', {})
+        return render(request, 'login.html', {})
 
 def logout_user(request):
-    pass
+    logout(request)
+    messages.success(request, 'You have successfully logged out')
+    return redirect('home')
