29af4fe188dc6dc7796e16b61bc1f6003305b7cd
N.Albertsen <sk74pn@studnet.auu.dk>
2024-10-31 09:15:56 +0100

    Models file, for generering af database, v.1 basal tid, email, ip, url.

--- django_central_server/django_server/website/models.py
+++ django_central_server/django_server/website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Blacklist(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField(max_length=255)
+    ip = models.CharField(max_length=15)
+    url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return (f"{self.ip} - {self.url}")
