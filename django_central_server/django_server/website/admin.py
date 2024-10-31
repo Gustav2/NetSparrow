18e0c1f8e3f1297cde94e1ec48e2a727aa3bc174
N.Albertsen <sk74pn@studnet.auu.dk>
2024-10-31 09:59:01 +0100

    Models blacklist is magratet, and now with user istead of email, as foreignkey

--- django_central_server/django_server/website/admin.py
+++ django_central_server/django_server/website/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Blacklist
 
+admin.site.register(Blacklist)
 # Register your models here.
--- django_central_server/django_server/website/models.py
+++ django_central_server/django_server/website/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Blacklist(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    email = models.EmailField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     ip = models.CharField(max_length=15)
     url = models.CharField(max_length=255)
 
     def __str__(self):
-        return (f"{self.ip} - {self.url}")
+        return f"{self.user.email} - {self.ip} - {self.url}"
