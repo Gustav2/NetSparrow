5c586cac9a7effcb239d4d32216988cee8cbd0f2
Viktor Hansen <viktoralmann@gmail.com>
2024-11-05 17:45:09 +0100

    removed old files

--- django_central_server/django_server/website/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Blacklist, MyBlacklist, CapturedPacket
-
-admin.site.register(Blacklist)
-admin.site.register(MyBlacklist)
-admin.site.register(CapturedPacket)
-
-# Register your models here.
