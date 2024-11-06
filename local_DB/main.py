0fcd6962196045121979c45541d0debbedc9eeaf
Magnus <mholms23@student.aau.dk>
2024-11-06 10:47:29 +0100

    local_db/main.py changed to http test, change to website caddy file

--- local_DB/main.py
+++ local_DB/main.py
@@ -1,4 +1,18 @@
-import api as v
+import requests
 
-print(v.speedtest(100000, random=True))
-v.flushDB('verify')
+# The API endpoint
+url = "https://netsparrow.viktorkirk.com/packet_capture/"
+
+# Data to be sent
+data = {
+    "ip" : "2.1.1.1",
+    "url" : "https://www.youtube.com",
+    "user_id" : 1,
+}
+
+# A POST request to the API
+for i in range (20):
+    response = requests.post(url, json=data)
+
+    # Print the response
+    print(response.json())
