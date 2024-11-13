fdb9d8421553e5fa1e17d566947b0596f93faf90
N.Albertsen <sk74pn@studnet.auu.dk>
2024-11-13 15:00:07 +0100

    Add to myblacklist now works, and the test file is added

---
+++ django_central_server/post_myblacklist.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+url = "https://netsparrow.viktorkirk.com/settings/add_to_myblacklist/"
+
+data = {
+    "ip": "444.444.444.005",
+    "url": "http://445.dk"
+}
+
+headers = {
+    "Authorization": "Token 71c08c598319cded9587ff36b0069226fd0565a1",
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, headers=headers, json=data) 
+
+print("Status Code:", response.status_code)
+print("Response JSON:", response.json())
