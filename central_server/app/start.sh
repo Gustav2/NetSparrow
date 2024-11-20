e7cf95805ceeaf02ef3fe18381bf09bb1573b1d4
Viktor Hansen <viktoralmann@gmail.com>
2024-11-20 13:32:48 +0100

    add migrations for website in start script

--- central_server/app/start.sh
+++ central_server/app/start.sh
@@ -2,6 +2,8 @@
 
 python manage.py makemigrations
 python manage.py migrate
+python manage.py makemigrations website
+python manage.py migrate website
 python manage.py collectstatic --noinput
 
 exec gunicorn --bind 0.0.0.0:8000 --workers 3 myproject.wsgi:application
