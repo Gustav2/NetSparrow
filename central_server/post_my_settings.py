8654f221a35bf24eed9762479fd06b5372a10183
N.Albertsen <sk74pn@studnet.auu.dk>
2024-11-26 13:26:52 +0100

    eks code for api settingsssssasdasd works now

--- central_server/post_my_settings.py
+++ central_server/post_my_settings.py
@@ -3,14 +3,14 @@ import requests
 url = "https://netsparrow.viktorkirk.com/api/settings/update/"
 
 headers = {
-    "Authorization": "f990deebf6b6f888560a4b2bc131989496a55030",
+    "Authorization": "Token f990deebf6b6f888560a4b2bc131989496a55030",
     "Content-Type": "application/json",
 }
 
 data = {
-    "auto_add_blacklist": True,
+    "auto_add_blacklist": False,
     "log_suspicious_packets": True,
-    "enable_ip_blocking": True,
+    "enable_ip_blocking": False,
     "dark_mode": True,
     "notify_blacklist_updates": True,
     "notify_suspicious_activity": True,
