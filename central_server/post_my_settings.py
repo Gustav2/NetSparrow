59ba6ddec1c3e1d1a457fc26142b3f471f2e729d
N.Albertsen <sk74pn@studnet.auu.dk>
2024-11-26 13:21:00 +0100

    eks code for api settings

--- central_server/post_my_settings.py
+++ central_server/post_my_settings.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "http://yourdomain.com/api/settings/update/"
+url = "https://netsparrow.viktorkirk.com/api/settings/update/"
 
 headers = {
     "Authorization": "Token your_token_here",
@@ -10,10 +10,10 @@ headers = {
 data = {
     "auto_add_blacklist": True,
     "log_suspicious_packets": True,
-    "enable_ip_blocking": False,
+    "enable_ip_blocking": True,
     "dark_mode": True,
     "notify_blacklist_updates": True,
-    "notify_suspicious_activity": False,
+    "notify_suspicious_activity": True,
 }
 
 response = requests.post(url, json=data, headers=headers)
