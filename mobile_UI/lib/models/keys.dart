0dda7c753491ed3a71087f65ee3829bc0a79f51b
Lakjer <klakje23@student.aau.dk>
2024-12-02 10:16:12 +0100

    added docs to data_from_api module

--- mobile_UI/lib/models/keys.dart
+++ mobile_UI/lib/models/keys.dart
@@ -1,4 +1,4 @@
-String blacklistIp = "https://netsparrow.viktorkirk.com";
-String ip = "https://piip.com";
+String blacklisturl = "https://netsparrow.viktorkirk.com";
+String piurl = "https://piip.com";
 int apiKey = 0;
 String blacklistToken = "Token f990deebf6b6f888560a4b2bc131989496a55030";
