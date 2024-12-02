fc6f047691d7a870a6e7bb7a984eff8c90e08026
Lakjer <klakje23@student.aau.dk>
2024-12-02 09:53:19 +0100

    .

--- mobile_UI/lib/main.dart
+++ mobile_UI/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/screens/home/home.dart';
 import 'package:my_app/theme.dart';
-import 'package:my_app/screens/login/login.dart';
 
 void main() {
   runApp(MaterialApp(
