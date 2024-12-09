3529c680d510b13db3b4f81cb810f28ce459922f
Lakjer <klakje23@student.aau.dk>
2024-12-09 14:00:55 +0100

    f

--- central_server/get_myblacklist_test.py
+++ central_server/get_myblacklist_test.py
@@ -3,7 +3,7 @@ import requests
 url = "https://netsparrow.viktorkirk.com/settings/myblacklist/"
 
 headers = {
-    "Authorization": "Token 71c08c598319cded9587ff36b0069226fd0565a1",
+    "Authorization": "Token f990deebf6b6f888560a4b2bc131989496a55030",
     "Content-Type": "application/json"
 }
 
--- central_server/post_capture_test.py
+++ central_server/post_capture_test.py
@@ -1,19 +1,19 @@
 import requests
-import json
+
 
 url = "https://netsparrow.viktorkirk.com/packet_capture/"
 
 data = {
-    "ip": "6.6.6.1",
+    "ip": "64.226.100.239",
     "url": "None"
 }
 
 headers = {
-    "Authorization": "Token 71c08c598319cded9587ff36b0069226fd0565a1",
+    "Authorization": "Token f990deebf6b6f888560a4b2bc131989496a55030",
     "Content-Type": "application/json"
 }
 
 response = requests.post(url, headers=headers, json=data) 
 
 print("Status Code:", response.status_code)
-print("Response JSON:", response.json())
+print("Response JSON:")
