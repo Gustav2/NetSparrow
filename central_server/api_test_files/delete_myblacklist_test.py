d99ac23cc26cf95f18f44280e9a60590e8a28329
N.Albertsen <sk74pn@student.aau.dk>
2024-12-17 15:15:23 +0100

    changed the tokens so all is on the admin

--- central_server/api_test_files/delete_myblacklist_test.py
+++ central_server/api_test_files/delete_myblacklist_test.py
@@ -6,7 +6,7 @@ url = "https://netsparrow.viktorkirk.com/settings/remove_from_myblacklist/"
 
 # User token for authentication
 headers = {
-    "Authorization": "Token 71c08c598319cded9587ff36b0069226fd0565a1",
+    "Authorization": "Token f990deebf6b6f888560a4b2bc131989496a55030",
     "Content-Type": "application/json"
 }
 data = {
--- central_server/api_test_files/post_myblacklist_test.py
+++ central_server/api_test_files/post_myblacklist_test.py
@@ -11,7 +11,7 @@ data = {
 
 # User token for authentication
 headers = {
-    "Authorization": "Token 71c08c598319cded9587ff36b0069226fd0565a1",
+    "Authorization": "Token f990deebf6b6f888560a4b2bc131989496a55030",
     "Content-Type": "application/json"
 }
 
